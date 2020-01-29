e8bd7a516ca68e32a0416f062c056b9f127fe68b
shineunji <dmswlckato@gmail.com>
2020-01-29 17:31:30 +0900

    aria 속성 추가

--- js/tab.js
+++ js/tab.js
@@ -1,9 +1,19 @@
-var panels = $('.board section');
-var tabs = $('.tab a');
+// var panels = $('.board section');
+// var tabs = $('.tab a');
 
-tabs.click(function(event) {
-    event.preventDefault();
-    panels.removeClass('board-act');
+// tabs.click(function(event) {
+//     event.preventDefault();
+//     panels.removeClass('board-act');
     
-    $(this).parent().parent().addClass('board-act') //.parent.parent 부모 두 단계를 올라감.
-});
+//     $(this).parent().parent().addClass('board-act') //.parent.parent 부모 두 단계를 올라감.
+// });
+
+
+var tabs = $('.tab-list [role="tab"]');
+
+tabs.on('click',function(e) {
+    e.preventDefault();
+    $(this).attr('aria-selected', true).siblings().attr('aria-selected', false);
+    var selectedId = "#"+$(this).attr('aria-controls');  
+    $(selectedId).addClass('section-act').siblings().removeClass('section-act');
+})
